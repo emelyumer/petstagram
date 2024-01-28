4370ae7a1e5a159e55204819a566b2c381b4f106
emelyumer <emi_92rs@abv.bg>
2024-01-28 19:29:31 +0200

    set up

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -1,1 +1,15 @@
-urlpatterns = ()
+from django.urls import path, include
+
+from petstagram.accounts.views import signup_user, signin_user, details_profile, edit_profile, delete_profile
+
+urlpatterns = (
+    path("signup/", signup_user, name="signup user"),
+    path("signin/", signin_user, name="signin user"),
+
+    path("profile/<int:pk>/", include([
+        path("", details_profile, name="details profile"),
+        path("edit/", edit_profile, name="edit profile"),
+        path("delete/", delete_profile, name="delete profile"),
+        ]),
+    )
+)
