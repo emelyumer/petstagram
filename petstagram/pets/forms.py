95a7f7eea90ba11b0a1cc8b3c890c667fb114bd9
emelyumer <emi_92rs@abv.bg>
2024-02-11 21:57:03 +0200

    set up

---
+++ petstagram/pets/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from petstagram.pets.models import Pet
+
+
+class PetBaseForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ('name', 'date_of_birth', 'pet_photo')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Pet name'}),
+            'date_of_birth': forms.DateInput(attrs={"type": "date"}),
+            'pet_photo': forms.URLInput(attrs={'placeholder': 'Link to image'})
+
+        }
+
+        labels = {
+            'name': 'Pet name',
+            'pet_photo': 'Link to image'
+        }
+
+
+class PetCreateForm(PetBaseForm):
+    pass
+
+
+class PetEditForm(PetBaseForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["date_of_birth"].widget.attrs['readonly'] = 'readonly'
+
+    def clean_date_of_birth(self):
+        # date_of_birth = self.cleaned_data["date_of_birth"]
+        # if date_of_birth != self.instance.date_of_birth:
+        #     raise ValidationError('Date of birth is readonly!')
+        #
+        # return date_of_birth
+        return self.instance.date_of_birth
+
+
+class PetDeleteForm(PetBaseForm):
+    pass
+
+
--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.pets.forms import PetCreateForm, PetEditForm
 from petstagram.pets.models import Pet
 
 
 def create_pet(request):
-    context = {}
+    pet_form = PetCreateForm(request.POST or None)
+
+    if request.method == 'POST':
+        if pet_form.is_valid():
+            created_pet = pet_form.save()
+            return redirect('details pet', username='Doncho', pet_slug=created_pet.slug)
+    context = {
+        'pet_form': pet_form,
+    }
     return render(request, "pets/create_pet.html", context)
+
+
+def edit_pet(request, username, pet_slug):
+    pet = Pet.objects.filter(slug=pet_slug).get()
+    pet_form = PetEditForm(request.POST or None, instance=pet)
+
+    if request.method == 'POST':
+        if pet_form.is_valid():
+            pet_form.save()
+            return redirect('details pet', username=username, pet_slug=pet_slug)
+    context = {
+        'pet_form': pet_form,
+        'username': username,
+        'pet': pet
+    }
+    return render(request, "pets/edit_pet.html", context)
 
 
 def details_pet(request, username, pet_slug):
@@ -20,6 +45,3 @@ def delete_pet(request, username, pet_slug):
     return render(request, "pets/delete_pet.html", context)
 
 
-def edit_pet(request, username, pet_slug):
-    context = {}
-    return render(request, "pets/edit_pet.html", context)
